e8ae3a5ccbe899dda5cf926660a95ec56631a893
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-05-06 13:53:00 -0500

    Initial Commit

---
+++ RemindersClone/RemindersCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersCloneApp.swift
+//  RemindersClone
+//
+//  Created by Sravanthi Chinthireddy on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
